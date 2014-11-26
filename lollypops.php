ae62a7930759b0adc3aa319099619022ad46eace
Jamie Burleigh <j.burleigh1@gmail.com>
2014-11-26 14:53:11 +0000

    support for posts, archives, tags, cats

--- lollypops.php
+++ lollypops.php
@@ -54,23 +54,16 @@ function my_plugin_menu() {
 
 function lollipop_options() {
 
-?>
-<div>
+echo '<div>
 <h2>Lollipop Trail</h2>
 A couple of options to customise the lollipop trail.
-<form action="options.php" method="post">
-<?php settings_fields('plugin_options'); ?>
-<?php do_settings_sections('plugin'); ?>
- 
-<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
-</form></div>
- 
- 
-<?php
-}?>
+<form action="options.php" method="post">';
+settings_fields('plugin_options');
+do_settings_sections('plugin');
+echo '<input name="Submit" type="submit" value="<?php esc_attr_e(\'Save Changes\'); ?>" />
+</form></div>';
 
-<?php
-
+}
 
 add_action( 'admin_menu', 'my_plugin_menu' );
 
@@ -81,23 +74,38 @@ function get_lollypops() {
 	$options = get_option('plugin_options');
 	$home =  $options['text_string'];
     $seperator =  $options['text_string2'];    
-
+    // Set the page title to current page, if exists.
+	$page_title = get_the_title($post->ID);
+    // Set the current parent to current parent page, if exists.
+    $parent_id = $post->post_parent;  
+    // Set the current post type, if exists
+    $posttype = get_post_type($post->ID);
+    // Set the current post type object, if exists.
+    $post_type_obj = get_post_type_object($posttype);
+    // Set the current post type title, if ecists.
+    $post_type_title = $post_type_obj->labels->name;
+    // Set the current post type permalink, if exists
+    $post_type_link = get_post_type_archive_link($posttype); 
+    // Set the current category title, if exists.
+    $category = single_cat_title("", false);
+    // Set the current taxonemy title, if exists.
+    $taxonomy = single_tag_title("", false);    
+    
 	$trail = '
     <ul class="lollipops">
 ';
     
+    // If current page is home, simply outout Home.
     if (!is_home()  && $home) {
         $trail .= '<li class="crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/" itemprop="url"><span itemprop="title">'.$home.'</span></a></li><span class="seperator">'.$seperator.'</span>';   
     }
     
-	$page_title = get_the_title($post->ID);
-    $parent_id = $post->post_parent;
-
+    // Conditionals for pages with no parents
     if (is_page()  && !$parent_id) {
-        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="' . get_permalink($page->ID) . '" itemprop="url"><span itemprop="title">' . $page_title . '</span></a></li>';
-    } elseif (is_archive()  && !$parent_id) {
-        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="/" itemprop="url"><span itemprop="title">' . post_type_archive_title('',false) . '</span></a></li>';
-    } elseif ( is_page() && $parent_id ) { 
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $page_title . '</span></li>';
+    } 
+    // Conditionals for pages with parents
+    elseif ( is_page() && $parent_id ) { 
         $breadcrumbs = array();
         $parent_id = $post->post_parent;
         while ($parent_id) {
@@ -108,7 +116,27 @@ function get_lollypops() {
         $breadcrumbs = array_reverse($breadcrumbs);
         foreach($breadcrumbs as $crumb) $trail .= $crumb;
         $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="' . get_permalink($page->ID) . '" itemprop="url"><span itemprop="title">' . $page_title . '</span></a></li>';
-    }
+    }    
+    //Conditionals for Archives
+    elseif (is_archive()  && !is_category() && !is_tag()) {
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $post_type_title . '</span></li>';
+    } 
+    // Conditionals for Cateory Archives
+    elseif (is_category()) {
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $category . '</span></li>';
+    } 
+    elseif (is_tag()) {
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $taxonomy . '</span></li>';
+    }     
+    // Conditionals for Regular Posts
+    elseif (is_single()  && ($posttype=='post')) {
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $page_title . '</span></li>';
+    } 
+    // Conditionals for Posts of Custom Post Type
+    elseif (is_single()  && !($posttype=='post')) {
+        $trail .= '<li class="crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="' . $post_type_link . '" itemprop="url"><span itemprop="title">' . $post_type_title . '</span></a></li><span class="seperator">'.$seperator.'</span>';
+        $trail .= '<li class="crumb active-crumb" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">' . $page_title . '</span></li>';
+    }     
 	$trail .= '
 </ul>
 ';
